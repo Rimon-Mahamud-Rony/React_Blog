bae4c5a42c43e4767ff1bd7e088cac95776e081b
Rimon Mahamud Rony <rimonronycste11@gmail.com>
2026-02-24 15:27:28 +0600

    ci: content page designed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,72 @@ import './App.css';
 import Header from './header';
 import Content from './content';
 import Footer from './footer';
+import { useState } from 'react';
 
 function App() {
+  const [Items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      itemName: "item one",
+    },
+    {
+      id: 2,
+      checked: false,
+      itemName: "item two",
+    },
+    {
+      id: 3,
+      checked: false,
+      itemName: "item three",
+    },
+    {
+      id: 4,
+      checked: true,
+      itemName: "item four",
+    },
+    {
+      id: 5,
+      checked: true,
+      itemName: "item five",
+    },
+    {
+      id: 6,
+      checked: false,
+      itemName: "item six",
+    },
+    {
+      id: 7,
+      checked: true,
+      itemName: "item seven",
+    },
+    {
+      id: 8,
+      checked: true,
+      itemName: "item eight",
+    },
+    {
+      id: 9,
+      checked: false,
+      itemName: "item nine",
+    },
+    {
+      id: 10,
+      checked: true,
+      itemName: "item ten",
+    },
+  ]);
+
+
+
   return (
     <div className="min-h-screen flex flex-col">
       <main className="flex-grow">
         <Header />
-        <Content />
+        <Content
+          Items={Items}
+          setItems = {setItems}
+        />
       </main>
       <Footer />
     </div>
--- src/content.jsx
+++ src/content.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import { AiFillDelete } from "react-icons/ai";
 
-const Content = () => {
+const Content = ({Items, setItems}) => {
     return (
       <div className="w-full flex justify-center p-2 mt-8">
-        <div className="w-3/4  bg-gray-100 py-1 px-4 border ">
-          <ul className="list-none">
-            <li>
-              <div className="flex flex-row w-full justify-between">
-                <div>checklist</div>
-                <div>items name</div>
-                <div>delete icon</div>
-              </div>
-            </li>
+        <div className="w-1/2 py-1 px-4">
+          <ul className="list-none flex flex-col gap-2 text-sm">
+            {Items.map((single_item) => (
+              <li
+                key={single_item.id}
+                className="border-1 borderborder border-indigo-600 p-4 rounded-sm"
+              >
+                <div className="flex flex-row w-full justify-between">
+                  <div>
+                    <input type="checkbox" checked={single_item.checked} />
+                  </div>
+                  <div>{single_item.itemName}</div>
+                  <div>
+                    <AiFillDelete className='text-xl hover:text-red-500 cursor-pointer'/>
+                  </div>
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
